75edcab5a8252d8590cedd4d53730ac04aa7148b
trf/conibj <issufi.badji@tty2000.com.br>
2025-09-17 15:00:51 -0300

    'commit'

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\ContactMail;
+use App\Models\Setting;
+use App\Http\Requests\ContactRequest;
+use Illuminate\Support\Facades\Mail;
+
+class ContactController extends Controller
+{
+    public function submit(ContactRequest $request)
+    {
+        $validated = $request->validated();
+        
+        $contact_email = Setting::select('contact_mail')->first()->value('contact_mail');
+
+        Mail::to($contact_email)->send(new ContactMail(
+            $validated['name'],
+            $validated['email'],
+            $validated['subject_mail'],
+            $validated['content']
+        ));
+
+        return to_route('home')->with('message', 'Message sent sucessfully !');
+    }
+}
